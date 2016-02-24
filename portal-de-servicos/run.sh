7151767dba451a28ba0fdde3e85511a5e9939ac1
Bruno Tavares <btavare@thoughtworks.com>
2016-02-24 13:12:32 -0300

    Remove comando que não será mais executado
    Como estamos repassando o processo para o processo filho, o comando
    `popd` não será mais executado.
    
    Esse commit remove o commando `popd` e troca o comando `pushd` para um
    comando `cd` por ser mais simples.

--- portal-de-servicos/run.sh
+++ portal-de-servicos/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pushd /var/lib/portal-de-servicos
-  exec /opt/portal-de-servicos/bin/portal-de-servicos
-popd
+cd /var/lib/portal-de-servicos
+exec /opt/portal-de-servicos/bin/portal-de-servicos
